d853534595a67747b8001446c542d524fb882d0a
lazypanda5050 <w.n.lazypanda5050@gmail.com>
2025-09-20 17:12:43 -0700

    simple stuff

--- Code/loginPRE.js
+++ Code/loginPRE.js
@@ -1,5 +1,4 @@
 (async function () {
-  alert("Hello Everybody! If you are not a developer, please do not use this version. Use this bookmarklet instead: javascript:(()=>{fetch('https://raw.githubusercontent.com/TheHumblePotato/Yap-Window/refs/heads/main/Code/login.js').then(r=>r.text()).then(eval);})();")
   if (document.getElementById("bookmarklet-gui")) {
     const gui = document.getElementById("bookmarklet-gui");
     gui.style.opacity = "1";
