2e3c9673608de520980171715297f222efc6bebe
shuweny30 <shuweny30@lakesideschool.org>
2025-10-11 08:46:32 -0700

    Update chat.js

--- Code/chat.js
+++ Code/chat.js
@@ -3268,7 +3268,7 @@
         for (const API_KEY of API_KEYS) {
           try {
             const response = await fetch(
-              "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
+              "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
                 API_KEY,
               {
                 method: "POST",
